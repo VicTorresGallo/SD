2f0dd4e723088f120f69eeb10004889439f93545
VicTorresGallo <vicentg1211@gmail.com>
2023-02-06 16:26:05 +0100

    Primer push

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ http.createServer( (request, response) => {
 console.log('Servidor ejecutándose en puerto 8080...');
 */
 'use strict'
+const port = process.env.PORT || 8888;
 const express = require('express');
-
 const app = express();
-
-app.get('/hola', (request, response) => {
-    response.send('Hola a todas y a todos desde Express!')
+app.get('/hola/:unNombre', (req, res) => {
+res.status(200).send({ mensaje: `Hola ${req.params.unNombre} desde SD!` });
 });
-
-app.listen(8080, () => {
-    console.log('API REST ejecutándose en http://localhost:8080/hola');
+app.listen(port, () => {
+console.log(`API REST ejecutándose en http://localhost:${port}/hola/:unNombre`);
 });
